7d8c274d03e71b471ba96620da2b6688a08a1824
JustinMitchell <mitchelljm04@gmail.com>
2023-03-13 14:24:25 -0400

    Render Deploy

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "application#index"
+  get "*path", to: "application#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
